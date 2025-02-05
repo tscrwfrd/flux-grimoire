1c303ab37dc36289fba37f76bfa9158ecff24f5f
Trevor Crawford <crawford.trevor@gmail.com>
2025-02-04 20:38:36 -0700

    Initial 1d shallow water equations with fct.

--- app/main.f90
+++ app/main.f90
@@ -1,10 +1,12 @@
 program main
   use fluid_forge, only: say_hello, fct
   use run1d, only: sim1d
+  use run1d_dam_break, only: dam1d
   implicit none
   integer :: val
 
   call say_hello()
-  val = sim1d()
 
+  val = sim1d()
+  val = dam1d()
 end program main
